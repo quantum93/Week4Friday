377e8c14badd102006aa2f40b234330791d4334f
Tae Lee <taebumlee@gmail.com>
2019-06-21 07:50:33 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// ------------------------------ business logic -------------------------------
+
+// --------------------------- User interface logic ----------------------------
+$(document).ready(function () {
+  $("").submit(function (event) {
+    event.preventDefault();
+  });
+  $("").click(function (event) {
+    event.preventDefault();
+  });
+});
